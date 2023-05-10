4402927bfdc3892cbf186de6c5132648d1ff893f
ignacio tosini <ignaciotosini2002@gmail.com>
2023-05-10 20:14:17 -0300

    Ahora si Commit entrega Final Cambio numero Pi...

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,19 @@
 import React, { useContext } from "react";
 import { DarkModeContext } from '../../context/DarkModeContext';
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   const { darkMode } = useContext(DarkModeContext);
-
+  const handleLinkClick = (event) => {
+    if (!event.ctrlKey || !event.shiftKey) {
+      // Si no se mantienen presionadas ambas teclas, no navegamos a /productmanagementpage
+      event.preventDefault();
+    }
+  };
   return (
     <div className={`banner ${darkMode ? "modo-oscuro" : ""}`}>
-      <p className="banner-text">¡Envío gratuito en todos los pedidos!</p>
+      <p className="banner-text">¡Envío gratuito en todos los pedidos</p>
+      <Link to={'/productmanagementpage'} onClick={handleLinkClick}><p className="link">!</p></Link>
     </div>
   );
 };
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const handleLinkClick = (event) => {
-    if (!event.ctrlKey || !event.shiftKey) {
-      // Si no se mantienen presionadas ambas teclas, no navegamos a /productmanagementpage
-      event.preventDefault();
-    }
-  };
   return (
     <div className="footer-container">
       <ul className="contact-info">
@@ -21,7 +15,6 @@ const Footer = () => {
         <a target="_blank" rel="noreferrer" href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
         <a target="_blank" rel="noreferrer" href="https://twitter.com/"><i className="fab fa-twitter"></i></a>
       </div>
-      <Link to={'/productmanagementpage'} onClick={handleLinkClick} className='pi '>π</Link>
     </div>
   );
 };
